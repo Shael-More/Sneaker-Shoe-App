206385f8431ec817f94ec7f5072e6fc301ec7c66
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-11-11 23:45:04 +0530

    add assets

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
+import { login, navlinks } from '../../constants/index';
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center  shadow-sm p-8'>
-      <div className='ml-3'>
+    <header className='flex justify-between items-center  shadow-sm p-8'>
+      <div className='px-8'>
         <NavLink to={'/'}>
           <div className='flex items-center'>
-           <img src={logo} alt='' className='w-[34px]' />
+            <img src={logo} alt='' className='w-[34px]' />
             <h1 className='text-2xl font-lato ml-2 font-bold'>Sneaker</h1>
           </div>
         </NavLink>
@@ -16,65 +17,30 @@ const Header = () => {
 
       <nav className='text-gray font-semibold'>
         <ul className='flex space-x-10 list-none '>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-orange' : 'text-gray'
-              }
-              to={'/'}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-orange' : 'text-gray'
-              }
-              to={'/shop'}
-            >
-              Shop
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-orange' : 'text-gray'
-              }
-              to={'/about'}
-            >
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-orange' : 'text-gray'
-              }
-              to={'/blog'}
-            >
-              Blog
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-orange' : 'text-gray'
-              }
-              to={'/contact'}
-            >
-              Contact
-            </NavLink>
-          </li>
+          {navlinks.map((link) => (
+            <li>
+              <NavLink
+                to={link.to}
+                className={({ isActive }) =>
+                  isActive ? 'text-orange' : 'text-gray'
+                }
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
 
-      <div className='flex space-x-3 mr-2 items-center text-gray'>
-        <p>Search</p>
-        <p>Cart</p>
-        <p>Profile</p>
+      <div className='flex space-x-3 px-8 items-center text-gray'>
+        {/* icons will come here */}
+        {
+          login.map(item => (
+            <p key={item.label}>{item.icon}</p>
+          ))
+        }
       </div>
-    </div>
+    </header>
   );
 };
 
---
+++ src/constants/index.js
@@ -0,0 +1,129 @@
+// import {
+//   facebook,
+//   instagram,
+//   twitter,
+//   shieldTick,
+//   support,
+//   truckFast,
+//   star,
+// } from '../assets/icons';
+// import {
+//   bigShoe1,
+//   bigShoe2,
+//   bigShoe3,
+//   shoe1,
+//   shoe2,
+//   shoe3,
+//   shoe4,
+//   collection1,
+//   collection2,
+//   best1,
+//   best2,
+//   best3,
+//   best4,
+//   best5,
+//   tech1,
+//   tech2,
+//   tech3,
+//   choice1,
+//   choice2,
+//   last1,
+//   last2,
+//   last3,
+// } from '../assets/images';
+
+import { search, cart, user } from '../assets/icons';
+
+export const navlinks = [
+  { to: '/', label: 'Home' },
+  { to: '/shop', label: 'Shop' },
+  { to: '/about', label: 'About' },
+  { to: '/blog', label: 'Blog' },
+  { to: '/contact', label: 'Contact' },
+];
+
+export const login = [
+  { label: 'search', icon: search },
+  { label: 'cart', icon: cart },
+  { label: 'user', icon: user },
+];
+
+// export const shoes = [
+//   { thumbnail: thumbnailShoe1, bigShoe: bigShoe1 },
+//   { thumbnail: thumbnailShoe2, bigShoe: bigShoe2 },
+//   { thumbnail: thumbnailShoe3, bigShoe: bigShoe3 },
+// ];
+
+// export const products = [
+//   {
+//     imgUrl: shoe1,
+//     name: 'Nike 1',
+//     price: '$200.20',
+//     reviews: '11.4k',
+//     sold: 'Sold Out 85%',
+//   },
+//   {
+//     imgUrl: shoe2,
+//     name: 'Nike 2',
+//     price: '$210.20',
+//     reviews: '11.6k',
+//     sold: 'Sold Out 85%',
+//   },
+//   {
+//     imgUrl: shoe3,
+//     name: 'Nike 3',
+//     price: '$220.20',
+//     reviews: '11.4k',
+//     sold: 'Sold Out 85%',
+//   },
+//   {
+//     imgUrl: shoe4,
+//     name: 'Nike 4',
+//     price: '$230.20',
+//     reviews: '11.4k',
+//     sold: 'Sold Out 85%',
+//   },
+// ];
+
+// export const bestSeller = [
+//   {
+//     img: best1,
+//     name: 'Cloudnova Sneaker',
+//     price: 45.0,
+//   },
+//   {
+//     img: best2,
+//     name: 'Urban Pulse',
+//     price: 59.0,
+//   },
+//   {
+//     img: best3,
+//     name: 'Downtown Dash',
+//     price: 25.0,
+//   },
+//   {
+//     img: best4,
+//     name: 'City Sprinter',
+//     price: 65.0,
+//   },
+//   {
+//     img: best5,
+//     name: 'Concrete Cruiser',
+//     price: 60.0,
+//   },
+// ];
+
+// export const technology = [
+//   { img: tech1, desc: 'Paddes Heel For Comfort And Grip' },
+//   {
+//     img: tech2,
+//     desc: "The Outside Cutout Patter, Inspird By The Atoms Logo, Help Reduce The Shoe's Weight",
+//   },
+//   { img: tech3, desc: 'Square EyeKets Keep Your Flat Laces Near And Stylish' },
+// ];
+
+// export const socialMedia = [
+//   { src: facebook, alt: 'facebook logo' },
+//   { src: twitter, alt: 'twitter logo' },
+//   { src: instagram, alt: 'instagram logo' },
+// ];
