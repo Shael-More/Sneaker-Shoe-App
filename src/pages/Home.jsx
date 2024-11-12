b75baa46880e8fa184e7f5e95369272501eb3716
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-11-12 23:48:34 +0530

    add Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="home" className="w-full min-h-screen">
+      <div className='relative flex flex-col justify-center items-start xl:w-2/5 w-full '>
+      <p>Our Summer Collection</p>
+      <h1>
+        <span>The New Arrival</span>
+        <br />
+        <span>Nike Shoes</span>
+      </h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, facere ratione. Delectus qui cum, labore temporibus omnis modi facilis numquam, quidem mollitia tempore maxime tempora illo non sunt, quas ducimus!</p>
+      </div>
+    </section>
   )
 }
 
