0f4f85452707ba61a646012ae2a73e8aafb83749
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-11-15 16:40:02 +0000

    changes in file

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import Shop from './pages/Shop';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Header from './components/Header';
+import Test from './JSONLY/Test'
 
 function App() {
   return (
     <>
-    <Header />
+    <Test />
+    {/* <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/about' element={<About />} />
         <Route path='/blog' element={<Blog />} />
         <Route path='/contact' element={<Contact />} />
-      </Routes>
+      </Routes> */}
     </>
   );
 }
